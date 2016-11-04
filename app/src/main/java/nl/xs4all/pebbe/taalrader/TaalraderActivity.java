6a286bb398c7b666465058e6f68f2aca095d3b0f
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-11-04 21:31:15 +0100

    knoppen blijven zichtbaar als toetsenbord in beeld komt; vertaling van taalcodes

--- app/src/main/java/nl/xs4all/pebbe/taalrader/TaalraderActivity.java
+++ app/src/main/java/nl/xs4all/pebbe/taalrader/TaalraderActivity.java
@@ -1,7 +1,6 @@
 package nl.xs4all.pebbe.taalrader;
 
 // TODO: info -> popup
-// TODO: knoppen moeten zichtbaar blijven als toestenbord wordt geopend
 // TODO: styles
 // TODO: launch icon
 // TODO: Go omzetten naar Java
@@ -45,10 +44,24 @@ public class TaalraderActivity extends AppCompatActivity {
         EditText ev = (EditText) findViewById(R.id.myText);
         String text = ev.getText().toString();
         String taal = Taalrader.raadtaal(text);
-        taal = taal.replace("\n", " ").replace(".utf8", "");
+        String[] langs = taal.replace(".utf8", "").split("\n");
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < langs.length; i++) {
+            if (i > 0) {
+                builder.append(", ");
+            }
+            builder.append(codeToLang(langs[i]));
+        }
+        taal = builder.toString();
         TextView tv = (TextView) findViewById(R.id.taal);
         tv.setText(taal);
-        // TODO: weergave van taal kan mooier
+    }
+
+    public void clear(View v) {
+        EditText ev = (EditText) findViewById(R.id.myText);
+        ev.setText("");
+        TextView tv = (TextView) findViewById(R.id.taal);
+        tv.setText("");
     }
 
     @Override
@@ -86,7 +99,7 @@ public class TaalraderActivity extends AppCompatActivity {
         if (text.startsWith("http://") || text.startsWith("https://")) {
             Thread myThread = new Thread(runnable);
             myThread.start();
-            v.setText("Loading... " + text);
+            v.setText(R.string.loading + text);
         } else {
             v.setText(text);
             doText();
@@ -116,11 +129,75 @@ public class TaalraderActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-        public void clear(View v) {
-        EditText ev = (EditText) findViewById(R.id.myText);
-        ev.setText("");
-        TextView tv = (TextView) findViewById(R.id.taal);
-        tv.setText("");
+    private String codeToLang(String langcode) {
+        String lang;
+        switch (langcode) {
+            case "SHORT": lang = getString(R.string.SHORT); break;
+            case "UNKNOWN": lang = getString(R.string.UNKNOWN); break;
+            case "af": lang = getString(R.string.af); break;
+            case "ar": lang = getString(R.string.ar); break;
+            case "bg": lang = getString(R.string.bg); break;
+            case "ca": lang = getString(R.string.ca); break;
+            case "cs": lang = getString(R.string.cs); break;
+            case "cy": lang = getString(R.string.cy); break;
+            case "da": lang = getString(R.string.da); break;
+            case "de": lang = getString(R.string.de); break;
+            case "el": lang = getString(R.string.el); break;
+            case "en": lang = getString(R.string.en); break;
+            case "eo": lang = getString(R.string.eo); break;
+            case "es": lang = getString(R.string.es); break;
+            case "et": lang = getString(R.string.et); break;
+            case "eu": lang = getString(R.string.eu); break;
+            case "fa": lang = getString(R.string.fa); break;
+            case "fi": lang = getString(R.string.fi); break;
+            case "fr": lang = getString(R.string.fr); break;
+            case "fy": lang = getString(R.string.fy); break;
+            case "ga": lang = getString(R.string.ga); break;
+            case "gd": lang = getString(R.string.gd); break;
+            case "he": lang = getString(R.string.he); break;
+            case "hi": lang = getString(R.string.hi); break;
+            case "hr": lang = getString(R.string.hr); break;
+            case "hu": lang = getString(R.string.hu); break;
+            case "ia": lang = getString(R.string.ia); break;
+            case "id": lang = getString(R.string.id); break;
+            case "is": lang = getString(R.string.is); break;
+            case "it": lang = getString(R.string.it); break;
+            case "ja": lang = getString(R.string.ja); break;
+            case "kk": lang = getString(R.string.kk); break;
+            case "ko": lang = getString(R.string.ko); break;
+            case "la": lang = getString(R.string.la); break;
+            case "lb": lang = getString(R.string.lb); break;
+            case "lt": lang = getString(R.string.lt); break;
+            case "lv": lang = getString(R.string.lv); break;
+            case "mk": lang = getString(R.string.mk); break;
+            case "ms": lang = getString(R.string.ms); break;
+            case "nl": lang = getString(R.string.nl); break;
+            case "no": lang = getString(R.string.no); break;
+            case "pl": lang = getString(R.string.pl); break;
+            case "pt": lang = getString(R.string.pt); break;
+            case "ro": lang = getString(R.string.ro); break;
+            case "ru": lang = getString(R.string.ru); break;
+            case "sk": lang = getString(R.string.sk); break;
+            case "sl": lang = getString(R.string.sl); break;
+            case "sq": lang = getString(R.string.sq); break;
+            case "sr-latin": lang = getString(R.string.sr); break;
+            case "sr": lang = getString(R.string.sr); break;
+            case "sv": lang = getString(R.string.sv); break;
+            case "sw": lang = getString(R.string.sw); break;
+            case "ta": lang = getString(R.string.ta); break;
+            case "tl": lang = getString(R.string.tl); break;
+            case "tr": lang = getString(R.string.tr); break;
+            case "uk": lang = getString(R.string.uk); break;
+            case "ur": lang = getString(R.string.ur); break;
+            case "vi": lang = getString(R.string.vi); break;
+            case "vo": lang = getString(R.string.vo); break;
+            case "yi": lang = getString(R.string.yi); break;
+            case "yo": lang = getString(R.string.yo); break;
+            case "zh": lang = getString(R.string.zh); break;
+            default:
+                lang = langcode;
+        }
+        return lang;
     }
 
 }
