bc9480676760493dc7bb4a8826045d6a5a23048c
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2016-11-10 12:08:34 +0100

    sluit soft keyboard bij raden van taal

--- app/src/main/java/nl/xs4all/pebbe/taalrader/TaalraderActivity.java
+++ app/src/main/java/nl/xs4all/pebbe/taalrader/TaalraderActivity.java
@@ -4,6 +4,7 @@ package nl.xs4all.pebbe.taalrader;
 // TODO: launch icon
 // TODO: publiceren op google play
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,6 +14,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -51,19 +53,21 @@ public class TaalraderActivity extends AppCompatActivity {
     private void doText() {
         if (textcat == null) {
             textView.setText(textcatError);
-            return;
-        }
-        String text = editText.getText().toString();
-        String[] langs = textcat.classify(text);
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < langs.length; i++) {
-            if (i > 0) {
-                builder.append(", ");
+        } else {
+            String text = editText.getText().toString();
+            String[] langs = textcat.classify(text);
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < langs.length; i++) {
+                if (i > 0) {
+                    builder.append(", ");
+                }
+                builder.append(codeToLang(langs[i]));
             }
-            builder.append(codeToLang(langs[i]));
+            String taal = builder.toString();
+            textView.setText(taal);
         }
-        String taal = builder.toString();
-        textView.setText(taal);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     public void clear(View v) {
@@ -115,16 +119,6 @@ public class TaalraderActivity extends AppCompatActivity {
         if (t == null) {
             return;
         }
-
-        // text from intent, so make scrolling easier
-        editText.setFocusable(false);
-        editText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                editText.setFocusableInTouchMode(true);
-                editText.requestFocus();
-            }
-        });
 
         final String text = t;
 
